552a99e7f4c20af712bff7a96c996a30a0caba42
Sandip <sandipramanik07@gmail.com>
2021-11-17 21:50:15 +0530

    Add notification service

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications_example/main.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+import '../details_page.dart';
+
+class NotificationService {
+  // Singleton pattern
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  NotificationService._internal();
+
+  static const channelId = "1";
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static final AndroidNotificationDetails _androidNotificationDetails =
+      AndroidNotificationDetails(
+    "channelId",
+    "channelName",
+    channelDescription: "This is channel description",
+    playSound: true,
+    priority: Priority.high,
+    importance: Importance.high,
+  );
+
+  static final IOSNotificationDetails _iOSNotificationDetails =
+      IOSNotificationDetails();
+
+  final NotificationDetails notificationDetails = NotificationDetails(
+    android: _androidNotificationDetails,
+    iOS: _iOSNotificationDetails,
+  );
+
+  Future<void> init() async {
+    final AndroidInitializationSettings androidInitializationSettings =
+        AndroidInitializationSettings("icon");
+
+    final IOSInitializationSettings iOSInitializationSettings =
+        IOSInitializationSettings(
+      defaultPresentAlert: false,
+      defaultPresentBadge: false,
+      defaultPresentSound: false,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iOSInitializationSettings,
+    );
+
+    // *** Initialize timezone here ***
+    tz.initializeTimeZones();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: onSelectNotification,
+    );
+  }
+
+  Future<void> requestIOSPermissions() async {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+
+  Future<void> showNotification(
+      int id, String title, String body, String payload) async {
+    await flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails,
+      payload: payload,
+    );
+  }
+
+  Future<void> scheduleNotification(int id, String title, String body,
+      DateTime eventDate, TimeOfDay eventTime, String payload,
+      [DateTimeComponents? dateTimeComponents]) async {
+    final scheduledTime = eventDate.add(Duration(
+      hours: eventTime.hour,
+      minutes: eventTime.minute,
+    ));
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      payload: payload,
+      matchDateTimeComponents: dateTimeComponents,
+    );
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+}
+
+Future<void> onSelectNotification(String? payload) async {
+  await navigatorKey.currentState
+      ?.push(MaterialPageRoute(builder: (_) => DetailsPage(payload: payload)));
+}
